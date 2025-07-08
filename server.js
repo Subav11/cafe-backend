3224582b6c4a532cd8c5d7d9ed0a484d5e2098a9
Subav11 <subhamyadav7528@gmail.com>
2025-07-08 15:28:20 +0530

    update

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8081, () => {
+//     console.log("Server started");
+//   });
+// });
+
+mongoose
+  .connect(
+    `mongodb+srv://${dbuser}:${dbpass}@cluster0.ts55vzb.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(8081, () => {
+      console.log("Server started");
+    });
+  });
+app.use("/api/users", userRouter);
