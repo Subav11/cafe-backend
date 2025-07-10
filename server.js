a9b9121feaef015327bdb0b2ea44fb89b2eef3d5
Subav11 <subhamyadav7528@gmail.com>
2025-07-10 15:58:53 +0530

    update

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 dotenv.config();
 
